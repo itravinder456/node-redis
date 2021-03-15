6e6bc1bdc4af7b5ea13aa4f3e6569e8f490116db
Ravinder Varikuppala <it.ravinder.456@gmail.com>
2021-03-15 23:04:23 +0530

    intial commit

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     redisEndPoint: process.env.REDIS_END_POINT_STRING_STAGING,
-    redisPort: process.env.REDIS_END_POINT_PORT
+    redisPort: process.env.REDIS_END_POINT_PORT,
+    redis_auth_token: process.env.REDIS_AUTH_TOKEN
 };
--- redis.js
+++ redis.js
@@ -1,9 +1,19 @@
 const config = require("./config")
 // create and connect redis client to server instance.
 console.log(config.redisEndPoint);
-var client = require('redis').createClient(config.redisPort, config.redisEndPoint, {
-    no_ready_check: true
-});
+// var client = require('redis').createClient(config.redisPort, config.redisEndPoint, {
+//     no_ready_check: true
+// });
+
+var client = require('redis').createClient(
+    {
+        no_ready_check: true,
+        host: config.redisEndPoint,
+        port: config.redisPort,
+        password: config.redis_auth_token
+    });
+
+
 
 // Print redis errors to the console
 client.on('error', (err) => {
