edab38957e84c79be1f6e26e04e003cab86a7cbf
Ravinder Varikuppala <it.ravinder.456@gmail.com>
2021-03-15 21:00:43 +0530

    intial commit

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const app = express();
 
 app.use(session({
     secret: 'mysecret$HFRYJJj218$$',
-    name: "_redis_test",
+    name: "session",
     // create new redis store.
     store: new redisStore({
-        host: config.redisEndPoint, port: config.redisPort, client: redisClient, ttl: 60 * 1 // in seconds (60 * 1 = 1 Minute)
+        client: redisClient, ttl: 60 * 1 // in seconds (60 * 1 = 1 Minute)
     }),
     saveUninitialized: false,
     resave: false
@@ -59,36 +59,6 @@ app.get("/logout", (req, res) => {
 // use response-time as a middleware
 app.use(responseTime());
 
-
-// create an api/search route
-app.get('/api/search', (req, res) => {
-    // Extract the query from url and trim trailing spaces
-    const query = (req.query.query).trim();
-    // Build the Wikipedia API url
-    const searchUrl = `https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&page=${query}`;
-
-    // Try fetching the result from Redis first in case we have it cached
-    return redisClient.get(`wikipedia:${query}`, (err, result) => {
-        // If that user exist in Redis store
-        if (result) {
-            const resultJSON = JSON.parse(result);
-            return res.status(200).json(resultJSON);
-        } else { // user does not exist in Redis store
-            // Fetch directly from Wikipedia API
-            return axios.get(searchUrl)
-                .then(response => {
-                    const responseJSON = response.data;
-                    // Save the Wikipedia API response in Redis store
-                    redisClient.setex(`wikipedia:${query}`, 3600, JSON.stringify({ source: 'Redis Cache', ...responseJSON, }));
-                    // Send JSON response to client
-                    return res.status(200).json({ source: 'Wikipedia API', ...responseJSON, });
-                })
-                .catch(err => {
-                    return res.json(err);
-                });
-        }
-    });
-});
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
