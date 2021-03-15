e14764df409ab9594f371a09fc13150291fd5034
Ravinder Varikuppala <it.ravinder.456@gmail.com>
2021-03-16 00:13:43 +0530

    intial commit3

--- redis.js
+++ redis.js
@@ -1,23 +1,23 @@
 const config = require("./config")
 // create and connect redis client to server instance.
 console.log(config.redisEndPoint);
-// var client = require('redis').createClient(config.redisPort, config.redisEndPoint, {
-//     no_ready_check: true
-// });
+var client = require('redis').createClient(config.redisPort, config.redisEndPoint, {
+    no_ready_check: true
+});
 
-var client = require('redis').createClient(
-    // config.redisPort, config.redisEndPoint,
-    {
-        no_ready_check: true,
-        host: config.redisEndPoint,
-        port: config.redisPort,
-        auth_pass: config.redis_auth_token,
-    }
-);
+// var client = require('redis').createClient(
+//     // config.redisPort, config.redisEndPoint,
+//     {
+//         no_ready_check: true,
+//         host: config.redisEndPoint,
+//         port: config.redisPort,
+//         auth_pass: config.redis_auth_token,
+//     }
+// );
 
-// client.auth(config.redis_auth_token, function (response) {
-//     console.log("response:", response)
-// })
+client.auth(config.redis_auth_token, function (response) {
+    console.log("response:", response)
+})
 // client.send_command('AUTH', [config.redis_auth_token]);
 
 
