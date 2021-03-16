2b901e5a44c4ab2b86d299323365c2e884ff30e3
Ravinder Varikuppala <it.ravinder.456@gmail.com>
2021-03-16 11:13:14 +0530

    changes1

--- redis.js
+++ redis.js
@@ -1,9 +1,12 @@
 const config = require("./config")
 // create and connect redis client to server instance.
 console.log(config.redisEndPoint);
-var client = require('redis').createClient(config.redisPort, config.redisEndPoint, {
-    no_ready_check: true
-});
+// var client = require('redis').createClient(config.redisPort, config.redisEndPoint, {
+//     no_ready_check: true
+// });
+
+const client = redis.createClient({ host: redisEndPoint, port: redisPort, auth_pass: 'BT786khgbvgf896Bhgff4ffdvb_gyujht6htc76', tls: { checkServerIdentity: () => undefined } })
+
 
 // Print redis errors to the console
 client.on('error', (err) => {
